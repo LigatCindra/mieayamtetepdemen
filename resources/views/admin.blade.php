a0bf55215145bba54ea7927bdc8eceaa58289b29
MikitakaShou <124781764+MikitakaShou@users.noreply.github.com>
2023-12-12 23:12:51 +0700

    Add files via upload

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,19 +3,20 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <div class="buttoncontainer ms-3 mt-3 mb-3 me-3">
-                <form action="/logout" method="post" class="m-6">
-                    @csrf
-                    <button type="submit" class="btn btn-primary ">Logout</button>
-                </form>
-            </div>
+<div class="buttoncontainer ms-3 mt-3 mb-3 me-3">
+    <form action="/logout" method="post" class="m-6">
+        @csrf
+        <button type="submit" class="btn btn-primary ">Logout</button>
+    </form>
+</div>
   <title>Admin</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="path/to/admin.css">
   <style>
     body {
-      background-color: #FFDAB9;
+      background-color: #FFDAB9; /* Peach-Orange background color */
     }
+
     .buttoncontainer {
       margin: 1rem;
     }
@@ -32,6 +33,17 @@
             <div class="form-group">
               <label for="nama_makanan">Nama Makanan:</label>
               <input type="text" name="nama_makanan" class="form-control" required>
+              <label for="id_kategori">Kategori Makanan:</label>
+              <select name="id_kategori" class="form-control">
+                @foreach ($categories as $category)
+                  <option value="{{ $category->id_kategori }}">{{ $category->nama_kategori }}</option>
+                @endforeach
+              </select>
+              <label for="deskripsi">Deskripsi Makanan:</label>
+              <input type="text" name="deskripsi" class="form-control" required>
+              <label for="harga">Harga Makanan:</label>
+              <input type="number" name="harga" class="form-control" required>
+              <input type="file" name="url_gambar" accept="image/png, image/jpeg" class="form-control-file mt-3">
             </div>
             <button type="submit" class="btn btn-primary">Create Makanan</button>
           </form>
@@ -57,20 +69,32 @@
                   <td>{{ $row->harga }}</td>
                   <td>{{ $row->kategori->nama_kategori }}</td>
                   <td>{{ $row->deskripsi }}</td>
-                  <td><img style="height: 250px; width: 250px;" src="{{ asset('storage/' . $row->url_gambar) }}" /></td>
+                  <td><img style="height:250px; width: 250px;" src="{{ asset('storage/' . $row->url_gambar) }}" /></td>
                   <td>
-                    <!-- Edit Form -->
                     <form method="POST" action="{{ route('makanan.edit', $row->id) }}" enctype="multipart/form-data">
                       @csrf
                       @method('PUT')
-                      <!-- ... Form Inputs ... -->
+                      <div class="form-group">
+                        <label for="nama_makanan">Nama Makanan:</label>
+                        <input type="text" name="nama_makanan" value="{{ $row->nama_makanan }}" class="form-control" required>
+                        <label for="id_kategori">Kategori Makanan:</label>
+                        <select name="id_kategori" class="form-control">
+                          @foreach ($categories as $category)
+                            <option value="{{ $category->id_kategori }}">{{ $category->nama_kategori }}</option>
+                          @endforeach
+                        </select>
+                        <label for="deskripsi">Deskripsi Makanan:</label>
+                        <input type="text" name="deskripsi" value="{{ $row->deskripsi }}" class="form-control" required>
+                        <label for="harga">Harga Makanan:</label>
+                        <input type="number" name="harga" value="{{ $row->harga }}" class="form-control" required>
+                        <input type="file" name="url_gambar" accept="image/png, image/jpeg" class="form-control-file mt-3">
+                      </div>
                       <button type="submit" class="btn btn-primary">Edit Makanan</button>
                     </form>
-                    <!-- Delete Form -->
-                    <form style="margin-left: 5px;" method="POST" action="{{ route('makanan.destroy', $row->id) }}">
+                    <form style="margin-left:5px;" method="POST" action="{{ route('makanan.destroy', $row->id) }}">
                       @csrf
                       @method('DELETE')
-                      <button type="delete" class="btn btn-danger">Delete Makanan</button>
+                      <button type="delete" class="btn btn-primary">Delete Makanan</button>
                     </form>
                   </td>
                 </tr>
@@ -82,8 +106,6 @@
     </div>
   </div>
 
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
